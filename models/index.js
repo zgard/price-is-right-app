9f0602f8ad2ec6b3dd2907dd420d2db2bb928919
Nick Howland <nhowland@outlook.com>
2020-05-14 13:45:56 -0400

    trying to get heroku DB working with sequelize

--- models/index.js
+++ models/index.js
@@ -12,11 +12,13 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
+console.log(process.env.DATABASE_URL);
+
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(console.log(process.env[config.use_env_variable], config));
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(console.log(config.database, config.username, config.password, config));
 }
 
 fs
