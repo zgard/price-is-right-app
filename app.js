3ad9f305392287bf42216542864fe33c9779ecfd
Nick Howland <nhowland@outlook.com>
2020-05-14 13:46:48 -0400

    trying to get heroku DB working with sequelize

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();  
 
-const sequelize = new Sequelize({
-    connectionString: process.env.DATABASE_URL,
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres'
 });
 
