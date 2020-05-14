12567e88db95e7ee7a9519c33172c71b9f5c9885
Nick Howland <nhowland@outlook.com>
2020-05-14 14:27:29 -0400

    trying to get heroku DB working with sequelize

--- config/config.js
+++ config/config.js
@@ -17,6 +17,9 @@ module.exports = {
         "dialect": "mysql"
     },
     "production": {
-        "use_env_variable": DATABASE_URL
+        "use_env_variable": process.env.DATABASE_URL,
+        "dialect": "postgres"
     }
 };
+
+console.log(process.env_DATABASE_URL);
