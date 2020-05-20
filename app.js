f75239f963b6f2b7814722ecbadb55a474b6ed59
Nick Howland <nhowland@outlook.com>
2020-05-19 21:08:37 -0400

    added logic for random math function; made ejs function

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 // https://nuxtjs.org/faq/heroku-deployment/ for alternative branch deployment
 // remove dotenv when running on heroku
 require('dotenv').config();  
+const apiKey = process.env.API_KEY;
+// const apiKey = 'insert here'; for hard-coded api key
+
 // const { Sequelize } = require('sequelize'); 
 
 // this works when running on heroku, but not locally
@@ -20,10 +23,6 @@ require('dotenv').config();
 //     }
 // });
 
-const apiKey = process.env.API_KEY;
-// const apiKey = 'insert here'; for hard-coded
-
-
 const axios = require('axios');
 const express = require('express');
 const app = express();
@@ -31,22 +30,93 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// working EJS function
-app.get('/game', function(req, res) {
-    // let randRecipe = {}; 
-    axios
-    .get(
-        // 'https://api.spoonacular.com/recipes/findByNutrients?maxCalories=300&number=5&apiKey=yourAPiKey' hard-coded
-        `'https://api.spoonacular.com/recipes/findByNutrients?maxCalories=300&number=5&apiKey=${apiKey}'`
-    )
-    .then(function (response) {
-        // console.log(response) 
-        const randRecipe = response;
-        // console.log(randRecipe) 
-        res.render('game', {recipeArray: randRecipe}); 
-    });
-});
+// working EJS function to create array of recipes
+// app.get('/game', function(req, res) {
+//     axios
+//     .get(
+//         // 'https://api.spoonacular.com/recipes/findByNutrients?maxCalories=300&number=5&apiKey=yourAPiKey' hard-coded
+//         `https://api.spoonacular.com/recipes/findByNutrients?maxCalories=300&number=5&apiKey=${apiKey}`
+//     )
+//     .then(function (response) {
+//         // console.log(response) 
+//         const randRecipe = response;
+//         // console.log(randRecipe) 
+//         res.render('game', {recipeArray: randRecipe}); 
+//     });
+// });
    
+// experimental code for wegman
+app.get('/products/', (req, res) => {
+    getProductWithWagman().then((product) => {
+        if (!product) {
+            return res.redirect('/products');
+        }
+        res.render('game', { product });
+    });
+})
+
+function randomInteger(array) {
+	return Math.floor(Math.random() * array.length);
+}
+
+function getProductWithWagman() {
+	const key = '&Subscription-key=c455d00cb0f64e238a5282d75921f27e';
+	const url = 'https://api.wegmans.io';
+	const categories = ['steak', 'milk', 'bread', 'fruits', 'soup', 'pasta'];
+	let sku = null;
+	const category = categories[randomInteger(categories)];
+	return axios
+		.get(
+			`${url}/products/search?query=${category}&api-version=2018-10-18${key}`
+		)
+		.then((results) => {
+			sku = results.data.results[randomInteger(results.data.results)].sku;
+			if (sku)
+				return Promise.all([
+					axios.get(
+						`${url}/products/${sku}/prices/68?api-version=2018-10-18${key}`
+					),
+					axios.get(`${url}/products/${sku}?api-version=2018-10-18${key}`),
+				]);
+		})
+		.then((results) => {
+			const product = {
+				sku,
+				pricing: results[0].data,
+                details: results[1].data,
+            };
+            console.log(product)
+            // console.log(product.pricing.stores[0].price) 
+			return product;
+		})
+		.catch((e) => console.error(e));
+}
+
+function createRandomPrices(price) {
+	const min = price - price * 0.2;
+	const max = price + price * 0.2;
+	const pricesSet = new Set(
+		[
+			price,
+			randomInteger(max, min, price),
+			randomInteger(max, min, price),
+			randomInteger(max, min, price),
+		].sort(() => Math.random() - 0.5)
+	);
+	if (pricesSet.size < 4) {
+		return createRandomPrices(price);
+	}
+	return Array.from(pricesSet);
+}function randomInteger(max, min, price) {
+	const cents = price.toString().split('.')[1];
+	const randomNumber =
+		Math.floor(Math.random() * (max - min) + min) + '.' + cents;
+	if (randomNumber !== price) {
+		return parseFloat(randomNumber);
+	}
+	return randomInteger(max, min, price);
+}const prices = createRandomPrices(19.75);
+
 app.listen('3000', function() {
     console.log('Listening on port 3000')
 });
@@ -108,79 +178,3 @@ app.listen('3000', function() {
 //       console.log('Express server listening on port ' + app.get('port'));
 //     });
 //   });
-
-
-// // first attempt function
-// // Render a recipe's calories for future game testing
-// function renderCalories(randRecipe) {
-//     // console.log(gameInfo); 
-//     let calorieInfo = randRecipe.map(calories => {
-//         // console.log(topGame); 
-//         // console.log(topGame.game.box.medium) 
-//         return`
-//             <div class="card mx-2 my-5">
-//                 <div class="card-body">
-//                     <h5 id="gameName" class="card-title d-flex flex-column text-center">${topGame.game.name}</h5>
-//                     <img class="img d-block w-100" src="${topGame.game.box.medium}" alt="...">
-//                     <span id="gameViewers" class="date badge badge-dark">${topGame.viewers} current viewers</span> 
-//                     <span id="gameViewers" class="date badge badge-dark">${topGame.channels} current streamers</span> 
-//                 </div>
-//             </div>
-//         `
-//     })
-//     return renderInfo.join('');
-// };
-
-// // Bring in 5 recipes and save to array
-// function getRecipe() {
-//     let randRecipe = {};
-//     axios
-//     .get(
-//         'https://api.spoonacular.com/recipes/findByNutrients?maxCalories=300&number=5&apiKey=ed320f4b389446a8bffffd4a53b1604c'
-//     )
-//     .then(function (response) {
-//         randRecipe = renderGame(response);
-//         // console.log(randRecipe); 
-// })};
-
-// getRecipe();  // this will later be a button you click to start the game 
-// // iterate over the array and return the calorie count of 5 recipes
-// function renderGame(recipeArray) {
-//     let recipeInfo = recipeArray.data.map(recipe => {
-//         return recipe.calories
-//     })
-//     console.log(recipeInfo);  
-// }
-
-// console.log(recipeInfo); 
-
-// second attempt (works)
-// app.get('/game', function(req, res) {
-//     // Get 5 recipes
-//     let randRecipe = {};
-//     axios
-//     .get(
-//         'https://api.spoonacular.com/recipes/findByNutrients?maxCalories=300&number=5&apiKey=ed320f4b389446a8bffffd4a53b1604c'
-//     )
-//     .then(function (response) {
-//         console.log(response)
-//         randRecipe = renderGame(response);
-//     });
-//     // Iterate over the array and return the calorie count of the recipes
-//     function renderGame(randRecipe) {
-//         let recipeInfo = randRecipe.data.map(recipe => {
-//             return`
-//             <div class="card" style="width: 18rem">
-//                 <div class="card-body">
-//                     <h5 class="card-title d-flex flex-column text-center">${recipe.calories}</h5>
-//                 </div>
-//             </div>
-//             `
-//             // return recipe.calories 
-//         })
-//         // return recipeInfo.join('');
-//         // console.log(recipeInfo);  
-//         // res.render('game', {recipeInfo});
-//     }
-// });
-
