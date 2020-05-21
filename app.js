3e118442ef627a5c3b4854c017c5483c33870292
Nick Howland <nhowland@outlook.com>
2020-05-21 17:46:26 -0400

    added working math logic w/ lodash functions; stylized card rendering

--- app.js
+++ app.js
@@ -23,6 +23,7 @@
 //     }
 // });
 
+const _ = require('lodash');
 const axios = require('axios');
 const express = require('express');
 const app = express();
@@ -45,21 +46,62 @@ app.set('views', 'views');
 //     });
 // });
    
-// work in progress code for wegman
+// Route to fetch a random product
+// app.get('/products/', (req, res) => {
+// 	getProductWithWagman()
+// 		.then((product) => {
+// 			if (!product) {
+// 				return res.redirect('/products');
+// 			}
+// 			// run the math function below to create random prices
+// 			const prices = createRandomPrices(product.pricing.price);
+// 			res.render('game', { product, prices });
+//     });
+// });
+
 app.get('/products/', (req, res) => {
-	getProductWithWagman()
-		.then((product) => {
-			if (!product) {
-				return res.redirect('/products');
-			}
-			const prices = createRandomPrices(product.pricing.price);
-			res.render('game', { product, prices });
+    getProductWithWagman().then((product) => {
+        if (!product) {
+            return res.redirect('/products');
+        }
+        res.render('game', { product });
     });
 });
 
 function randomInteger(array) {
 	return Math.floor(Math.random() * array.length);
 };
+
+// function getProductWithWagman() {
+// 	const key = '&Subscription-key=c455d00cb0f64e238a5282d75921f27e';
+// 	const url = 'https://api.wegmans.io';
+// 	const categories = ['steak', 'milk', 'bread', 'fruits', 'soup', 'pasta'];
+// 	let sku = null;
+// 	const category = categories[randomInteger(categories)];
+// 	return axios
+// 		.get(
+// 			`${url}/products/search?query=${category}&api-version=2018-10-18${key}`
+// 		)
+// 		.then((results) => {
+// 			sku = results.data.results[randomInteger(results.data.results)].sku;
+// 			if (sku)
+// 				return Promise.all([
+// 					axios.get(
+// 						`${url}/products/${sku}/prices/68?api-version=2018-10-18${key}`
+// 					),
+// 					axios.get(`${url}/products/${sku}?api-version=2018-10-18${key}`),
+// 				]);
+// 		})
+// 		.then((results) => {
+// 			const product = {
+// 				sku,
+// 				pricing: results[0].data,
+//                 details: results[1].data,
+// 			};
+// 			return product; 
+// 		})
+// 		.catch((e) => console.error(e));
+// };
 
 function getProductWithWagman() {
 	const key = '&Subscription-key=c455d00cb0f64e238a5282d75921f27e';
@@ -85,30 +127,47 @@ function getProductWithWagman() {
 			const product = {
 				sku,
 				pricing: results[0].data,
-                details: results[1].data,
-			};
-			return product; 
+				details: results[1].data,
+				prices: []
+            };
+			return product;
+		})
+		.then((product) => {
+			product['prices'].push(createRandomPrices(product));
+			return product;
 		})
 		.catch((e) => console.error(e));
+}
+
+function createRandomPrices(product) {
+	const price1 = product.pricing.price;
+	const price2 = _.round(price1 - .2, [precision=2]);
+	const price3 = _.round(price1 + 1, [precision=2]);
+	const price4 = _.round(price1 + 2, [precision=2]);
+	const pricesSet = [price1, price2, price3, price4];
+	_.shuffle(pricesSet);
+	return pricesSet;
 };
 
-function createRandomPrices(price) {
-	// return [1, 2, 3, 4]; this works
-	const min = price - price * 0.2;
-	const max = price + price * 0.2;
-	const pricesSet = new Set(
-		[
-			price,
-			randomInteger(max, min, price),
-			randomInteger(max, min, price),
-			randomInteger(max, min, price),
-		].sort(() => Math.random() - 0.5)
-	);
-	if (pricesSet.size < 4) {
-		return createRandomPrices(price);
-	}
-	return Array.from(pricesSet);
-};
+// Alex's function
+// function createRandomPrices(price) { 
+// 	// return [1, 2, 3, 4]; this works
+// 	const min = price - price * 0.2;
+// 	const max = price + price * 0.2;
+// 	const pricesArray = (
+// 		[
+// 			price,
+// 			randomInteger(max, min, price),
+// 			randomInteger(max, min, price),
+// 			randomInteger(max, min, price),
+// 		].sort(() => Math.random() - 0.5)
+// 	);
+// 	// console.log(pricesSet); 
+// 	if (pricesSet.size < 4) {
+// 		return createRandomPrices(price);
+// 	}
+// 	return Array.from(pricesSet);
+// };
 
 // }function randomInteger(max, min, price) {
 // 	const cents = price.toString().split('.')[1];
@@ -125,6 +184,8 @@ app.listen('3000', function() {
     console.log('Listening on port 3000')
 });
 
+
+// Databse code
 // Readline
 // const readline = require('readline');
 // const rl = readline.createInterface({
