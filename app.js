84088464cb36f3d26d493a8627954a1cb627464a
Nick Howland <nhowland@outlook.com>
2020-05-22 17:26:43 -0400

    cleaned up code in app.js; moved unused code into unusedcode.js file for now

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 // this is setup for Heroku
 // https://nuxtjs.org/faq/heroku-deployment/ for alternative branch deployment
+
 // remove dotenv when running on heroku
 require('dotenv').config();  
 // const apiKey = process.env.API_KEY;
-// const apiKey = 'insert here'; for hard-coded api key
 
+// make sure sequelize is initialized above the new Seuquelize object below
 const { Sequelize } = require('sequelize'); 
 
 // this works when running on heroku, but not locally
 // const sequelize = new Sequelize(process.env.DATABASE_URL); 
 
-// use this code when running locally
-// also you will need to run 'sudo apt-get install -y libpq-dev' and 'npm install pg-native'
+// Use this code when running locally
+// also you may need to run 'sudo apt-get install -y libpq-dev' and 'npm install pg-native'
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
@@ -29,40 +30,24 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 
+// Connect to sequelize database object
+const db = require('./models') 
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// sequelize models and tables
+db.users = require('./models/users.js')(sequelize, Sequelize); 
+module.exports = db; 
+
+// Body parser for responses
 app.use(bodyParser.urlencoded({ extended: false })) // parse application/json
 app.use(bodyParser.json())
 
+// EJS view files
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// working EJS function to create array of recipes
-// app.get('/game', function(req, res) {
-//     axios
-//     .get(
-//         // 'https://api.spoonacular.com/recipes/findByNutrients?maxCalories=300&number=5&apiKey=yourAPiKey' hard-coded
-//         `https://api.spoonacular.com/recipes/findByNutrients?maxCalories=300&number=5&apiKey=${apiKey}`
-//     )
-//     .then(function (response) {
-//         // console.log(response) 
-//         const randRecipe = response;
-//         // console.log(randRecipe) 
-//         res.render('game', {recipeArray: randRecipe}); 
-//     });
-// });
-   
-// Old route to fetch a random product
-// app.get('/products/', (req, res) => {
-// 	getProductWithWagman()
-// 		.then((product) => {
-// 			if (!product) {
-// 				return res.redirect('/products');
-// 			}
-// 			// run the math function below to create random prices
-// 			const prices = createRandomPrices(product.pricing.price);
-// 			res.render('game', { product, prices });
-//     });
-// });
-
+// main express route where game is played and JSON info is fetched from Wegman API
 app.get('/products', (req, res) => {
     getProductWithWagman().then((product) => {
         if (!product) {
@@ -72,14 +57,16 @@ app.get('/products', (req, res) => {
     });
 });
 
+
+// globals for use with express answer logging in below route
 let numCorrect = 0;
 let numIncorrect = 0;
 let totalAnswered = 0;
 let userAverage = 0;
-// let userAnswer = null; 
+// let userAnswer = null; don't think we need a truse/false condition for answers
 
 app.post('/answer/', (req, res) => {
-	// req should contain chosen answer. 
+	// req contains chosen answer, but price array still isn't sorted
 	var answer = req.body.answer
 	var correctPrice = req.body.correctPrice
 	console.log('your answer is: ' + answer)
@@ -89,6 +76,7 @@ app.post('/answer/', (req, res) => {
 		console.log('your total correct: ' + numCorrect)
 		console.log('you hit the correct answer!')
 	}
+	// don't think we need a true-false condition check
 	// if(userAnswer) {
 	// 	numCorrect++
 	// 	console.log(numCorrect)
@@ -102,6 +90,7 @@ app.post('/answer/', (req, res) => {
 	res.redirect('/products')
 });
 
+// route after user clicks end game button in game.ejs; tally up scores, average, and post to database
 app.post('/completed/', (req, res) => {
 	totalAnswered = numCorrect + numIncorrect;
 	console.log('you answered ' + totalAnswered + ' in total');
@@ -109,7 +98,7 @@ app.post('/completed/', (req, res) => {
 	userAverage = numCorrect / totalAnswered;
 	console.log('your average was ' + userAverage);
 	var completed = req.body.endGame
-	if (completed == req.body.endGame) {
+	if (completed) {
 		db.users.findOrCreate({
 			// see comments below
 			where:
@@ -131,13 +120,16 @@ app.post('/completed/', (req, res) => {
 					console.log('This entry was already made')
 				}
 			})
+			.then(res.redirect('/products')) // this should redirect to the dashboard where it displays the user stats.
 	}
 });
 
+// this function is used below to randomize values
 function randomInteger(array) {
 	return Math.floor(Math.random() * array.length);
 };
 
+// main API function; returns JSON of product info then randomizes it/shuffles. 
 function getProductWithWagman() {
 	const key = '&Subscription-key=c455d00cb0f64e238a5282d75921f27e';
 	const url = 'https://api.wegmans.io';
@@ -181,6 +173,7 @@ function getProductWithWagman() {
 		.catch((e) => console.error(e));
 };
 
+// randomize prices retrieved in above function; create shuffled array to send to game.ejs
 function createRandomPrices(product) {
 	const price1 = product.pricing.price;
 	const price2 = _.round(price1 - .2, [precision=2]);
@@ -192,130 +185,7 @@ function createRandomPrices(product) {
 	return pricesSet;
 };
 
+// hosting on port 3000
 app.listen('3000', function() {
     console.log('Listening on port 3000')
 });
-
-// Database code
-
-// Readline
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
-
-// // Connect to database object
-const db = require('./models') 
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
-
-// // Models and tables
-db.users = require('./models/users.js')(sequelize, Sequelize); 
-
-module.exports = db; 
-
-// // Test prompt user for parameters
-// rl.question('user name? ', (usernameInput) => {
-//     rl.question('please enter email ', (emailInput) => {
-//         rl.question('please enter total correct ', (userCorrect) => {
-//             rl.question('please enter total wrong ', (userWrong) => {
-//                 rl.question('please enter average ', (userAvg) => {
-//                     rl.close();
-//                 // create the user
-//                 db.users.findOrCreate({
-//                     where:
-//                         {
-//                         userName: usernameInput,
-//                         email: emailInput,
-//                         totalCorrect: userCorrect,
-//                         totalWrong: userWrong,
-//                         average: userAvg
-//                         }
-//                     })
-//                     .spread(function(userAdded, created) {
-//                         console.log(userAdded.get({
-//                             plain: true
-//                         }))
-//                         if (created) {
-//                             console.log('New user was added to database')
-//                         } else {
-//                             console.log('User already exists')
-//                         }
-//                     })
-//                 })
-//             })
-//         })
-//     })
-// });
-
-// // listen for requests
-// db.sequelize.sync().then(function() {
-//     http.createServer(app).listen(app.get('port'), function(){
-//       console.log('Express server listening on port ' + app.get('port'));
-//     });
-//   });
-
-
-// old function (no longer used)
-// function getProductWithWagman() {
-// 	const key = '&Subscription-key=c455d00cb0f64e238a5282d75921f27e';
-// 	const url = 'https://api.wegmans.io';
-// 	const categories = ['steak', 'milk', 'bread', 'fruits', 'soup', 'pasta'];
-// 	let sku = null;
-// 	const category = categories[randomInteger(categories)];
-// 	return axios
-// 		.get(
-// 			`${url}/products/search?query=${category}&api-version=2018-10-18${key}`
-// 		)
-// 		.then((results) => {
-// 			sku = results.data.results[randomInteger(results.data.results)].sku;
-// 			if (sku)
-// 				return Promise.all([
-// 					axios.get(
-// 						`${url}/products/${sku}/prices/68?api-version=2018-10-18${key}`
-// 					),
-// 					axios.get(`${url}/products/${sku}?api-version=2018-10-18${key}`),
-// 				]);
-// 		})
-// 		.then((results) => {
-// 			const product = {
-// 				sku,
-// 				pricing: results[0].data,
-//                 details: results[1].data,
-// 			};
-// 			return product; 
-// 		})
-// 		.catch((e) => console.error(e));
-// };
-
-// Alex's random math function (no longer used)
-// function createRandomPrices(price) { 
-// 	// return [1, 2, 3, 4]; this works
-// 	const min = price - price * 0.2;
-// 	const max = price + price * 0.2;
-// 	const pricesArray = (
-// 		[
-// 			price,
-// 			randomInteger(max, min, price),
-// 			randomInteger(max, min, price),
-// 			randomInteger(max, min, price),
-// 		].sort(() => Math.random() - 0.5)
-// 	);
-// 	// console.log(pricesSet); 
-// 	if (pricesSet.size < 4) {
-// 		return createRandomPrices(price);
-// 	}
-// 	return Array.from(pricesSet);
-// };
-
-// }function randomInteger(max, min, price) {
-// 	const cents = price.toString().split('.')[1];
-// 	const randomNumber =
-// 		Math.floor(Math.random() * (max - min) + min) + '.' + cents;
-// 	if (randomNumber !== price) {
-// 		return parseFloat(randomNumber);
-// 	}
-// 	return randomInteger(max, min, price);
-// }
-// }const prices = createRandomPrices(productPrice);
