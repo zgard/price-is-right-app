f5e6481e8cdaaad7e51bcd4c04147c5034b0f4f7
klester01 <klester01@aol.com>
2020-05-22 09:32:44 -0400

    added nodemon server.js script to auto start script when refreshing

--- server.js
+++ server.js
@@ -8,46 +8,47 @@ const db = require('./models');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(session({secret: 'abcdefg', resave: false, saveUninitialized: false}));
+app.use(session({ secret: 'abcdefg', resave: false, saveUninitialized: false }));
 
-passport.use(new googleStrategy ({
+passport.use(new googleStrategy({
     clientID: process.env.GOOGLE_CLIENTID,
     clientSecret: process.env.GOOGLE_SECRETID,
     callbackURL: 'http://localhost:5000/auth/google/callback'
-}, function(accessToken, refreshToken, profile, cb) {
-    db.users.findOrCreate({ where: {email: profile.emails[0].value, userName: profile.displayName} }).then(user => {
+}, function (accessToken, refreshToken, profile, cb) {
+    db.users.findOrCreate({ where: { email: profile.emails[0].value, userName: profile.displayName } }).then(user => {
         if (user) {
             return cb(null, user);
         }
     })
     console.log(profile);
-    }
+}
 ));
 
 //make sure to always put the initialize before the passport.session
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
+//passport.deserializeUser(function(user, done) {
+//    done(null, user);
+//});
 
 
-app.get('/auth/google', 
-    passport.authenticate('google', { scope: ['profile','email'] }));
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 
-app.get('/auth/google/callback', 
-    passport.authenticate('google', {failureRedirect: '/login'}),
-    function(req, res) {
+<<<<<<< Updated upstream
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
         res.redirect('/login');
-});
+    });
 
-app.get('/logout', function(req, res, next) {
+app.get('/logout', function (req, res, next) {
     console.log('logging out');
     req.logout();
     res.redirect('/logged-out');
