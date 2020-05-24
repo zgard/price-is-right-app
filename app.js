929998289d60861c29dfc0b9f44c7a5df3c1e144
Nick Howland <nhowland@outlook.com>
2020-05-23 20:44:21 -0400

    updated heroku DB to not use camelCase naming

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.use(new googleStrategy({
     	db.users.findOrCreate({ 
 			where: {
 					email: profile.emails[0].value, 
-					userName: profile.displayName
+					user_name: profile.displayName
 					} 
 		}).then(user => {
         	if (user) {
@@ -145,12 +145,12 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
 		const hashedPassword = await bcrypt.hash(req.body.password, 10) //includes await since we are using async
         db.users.create({
-            userName: req.body.name,
+            user_name: req.body.name,
             email: req.body.email,
             password: hashedPassword
         })
         .then(newUser => {
-        console.log(`New user ${newUser.userName}, with id ${newUser.id} has been created.`);
+        console.log(`New user ${newUser.user_name}, with id ${newUser.id} has been created.`);
         res.redirect('/login')//If everthing is correct, redirect user to login page to continue loggin in
         }).catch(e => {
             res.render('register', {error: 'This email already has a user account.'})
@@ -250,7 +250,7 @@ app.post('/completed/', (req, res) => {
 		// FROM users
 		// WHERE email=req.user.email;
 		// currently working function, but the overall user average is not being updated after each game
-		db.users.update({totalCorrect: numCorrect, totalWrong: numIncorrect, average: userAverage}, {
+		db.users.update({total_correct: numCorrect, total_wrong: numIncorrect, average: userAverage}, {
 				where: {
 					email:req.user.email
 				}
