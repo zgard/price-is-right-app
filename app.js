42d1c1a36119bfb75d80eeed5679450178c8ef8c
Shankar Pushparaj <shankarp88@gmail.com>
2020-05-25 18:30:17 -0400

    Login page changes

--- app.js
+++ app.js
@@ -38,8 +38,7 @@ const express = require('express');
 const app = express();
 
 // Zach's passport code
-if (process.env.NODE_ENV !== 'production')
-{
+if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 };
 
@@ -77,62 +76,52 @@ passport.use(new googleStrategy({
 	clientSecret: process.env.GOOGLE_SECRETID,
 	callbackURL: 'http://localhost:5000/auth/google/callback'
 },
-	function (accessToken, refreshToken, profile, done) 
-	{
+	function (accessToken, refreshToken, profile, done) {
 		// userEmail = profile.emails[0].value; 
 		db.users.findOrCreate({
 			where: {
-				email: profile.emails[ 0 ].value,
+				email: profile.emails[0].value,
 				userName: profile.displayName
 			}
-		}).then(user =>
-		{
-			if (user)
-			{
-				return done(null, user[ 0 ]);
+		}).then(user => {
+			if (user) {
+				return done(null, user[0]);
 			}
 		})
 	}
 ));
 // Check to authenticate if a user is logged in. If not, redirects user to login page
-function checkAuthenticated(req, res, next)
-{
-	if (req.isAuthenticated())
-	{
+function checkAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
 		return next()
 	}
 	res.redirect('/login')
 }
 // Make sure no uers dont go back to the login page if they are already authenticated
-function checkNotAuthenticated(req, res, next)
-{
-	if (req.isAuthenticated())
-	{
+function checkNotAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
 		return res.redirect('/dashboard')
 	}
 	next()
 }
 
-function logRequest(req, res, next)
-{
+function logRequest(req, res, next) {
 	console.log('another request');
 	next();
 };
 
 app.get('/auth/google',
-	passport.authenticate('google', { scope: [ 'profile', 'email' ] })
+	passport.authenticate('google', { scope: ['profile', 'email'] })
 )
 
 app.get('/auth/google/callback',
 	passport.authenticate('google', { failureRedirect: '/login' }),
-	function (req, res)
-	{
+	function (req, res) {
 		console.log("whatever");
 		res.redirect('/login');
 	});
 
-app.get('/login', checkNotAuthenticated, (req, res) =>
-{
+app.get('/login', checkNotAuthenticated, (req, res) => {
 	res.render('login.ejs')
 });
 
@@ -147,32 +136,26 @@ app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
 },
 ))
 
-app.get('/dashboard', checkAuthenticated, logRequest, (req, res, next) =>
-{
+app.get('/dashboard', checkAuthenticated, logRequest, (req, res, next) => {
 	res.render('dashboard');
 })
 
-app.get('/register', checkNotAuthenticated, (req, res) =>
-{
+app.get('/register', checkNotAuthenticated, (req, res) => {
 	res.render('register.ejs', { error: null })
 });
 
-app.post('/register', checkNotAuthenticated, async (req, res) =>
-{
-	try
-	{
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+	try {
 		const hashedPassword = await bcrypt.hash(req.body.password, 10) //includes await since we are using async
 		db.users.create({
 			userName: req.body.name,
 			email: req.body.email,
 			password: hashedPassword
 		})
-			.then(newUser =>
-			{
+			.then(newUser => {
 				console.log(`New user ${newUser.userName}, with id ${newUser.id} has been created.`);
 				res.redirect('/login')//If everthing is correct, redirect user to login page to continue loggin in
-			}).catch(e =>
-			{
+			}).catch(e => {
 				res.render('register', { error: 'This email already has a user account.' })
 			})
 	} catch {
@@ -184,8 +167,7 @@ app.post('/register', checkNotAuthenticated, async (req, res) =>
 // Create logout function. This function is provided by passport. Envoked using methodOverride
 // Install methodOverride library and require & use
 
-app.delete('/logout', (req, res) =>
-{
+app.delete('/logout', (req, res) => {
 	req.logOut()
 	res.redirect('/login')
 })
@@ -206,14 +188,13 @@ app.use(bodyParser.json())
 // EJS view files
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: false })); //this allows for the fields (password/email) on the form page to be access inside the req variable inside the login POST method
+app.use(flash())
 // Express route where game is played and JSON info is fetched from Wegman API
-app.get('/products', (req, res) =>
-{
-	getProductWithWagman().then((product) =>
-	{
-		if (!product)
-		{
+app.get('/products', (req, res) => {
+	getProductWithWagman().then((product) => {
+		if (!product) {
 			console.log("redirected");
 			return res.redirect('/products');
 
@@ -229,21 +210,18 @@ let totalAnswered = 0;
 let userAverage = 0;
 // Let userAnswer = null; don't think we need a truse/false condition for answers
 
-app.post('/answer/', (req, res) =>
-{
+app.post('/answer/', (req, res) => {
 	console.log(req.user)
 	var answer = req.body.answer
 	var correctPrice = req.body.correctPrice
 	console.log('your answer is: ' + answer)
 	console.log('the correct price: ' + correctPrice);
-	if (answer == correctPrice)
-	{
+	if (answer == correctPrice) {
 		numCorrect++
 		console.log('your total correct: ' + numCorrect)
 		console.log('you hit the correct answer!')
 	}
-	else
-	{
+	else {
 		numIncorrect++
 		console.log('your total incorrect: ' + numIncorrect)
 		console.log("you hit the incorrect answer!")
@@ -253,16 +231,14 @@ app.post('/answer/', (req, res) =>
 });
 
 // Route after user clicks end game button in game.ejs; tally up scores, average, and post to database
-app.post('/completed/', (req, res) =>
-{
+app.post('/completed/', (req, res) => {
 	totalAnswered = numCorrect + numIncorrect;
 	console.log('you answered ' + totalAnswered + ' in total');
 	console.log('you got ' + numCorrect + ' correct')
 	userAverage = numCorrect / totalAnswered;
 	console.log('your average was ' + userAverage);
 	var completed = req.body.endGame
-	if (completed)
-	{
+	if (completed) {
 
 		db.users.update({ totalCorrect: numCorrect, totalWrong: numIncorrect, average: userAverage }, {
 			where: {
@@ -285,26 +261,23 @@ app.post('/completed/', (req, res) =>
 });
 
 // This function is used below to randomize values
-function randomInteger(array)
-{
+function randomInteger(array) {
 	return Math.floor(Math.random() * array.length);
 };
 
 // Main API function; returns JSON of product info then randomizes it/shuffles. 
-function getProductWithWagman()
-{
+function getProductWithWagman() {
 	const key = '&Subscription-key=c455d00cb0f64e238a5282d75921f27e';
 	const url = 'https://api.wegmans.io';
-	const categories = [ 'steak', 'milk', 'bread', 'fruits', 'soup', 'pasta' ];
+	const categories = ['steak', 'milk', 'bread', 'fruits', 'soup', 'pasta'];
 	let sku = null;
-	const category = categories[ randomInteger(categories) ];
+	const category = categories[randomInteger(categories)];
 	return axios
 		.get(
 			`${url}/products/search?query=${category}&api-version=2018-10-18${key}`
 		)
-		.then((results) =>
-		{
-			sku = results.data.results[ randomInteger(results.data.results) ].sku;
+		.then((results) => {
+			sku = results.data.results[randomInteger(results.data.results)].sku;
 			if (sku)
 				return Promise.all([
 					axios.get(
@@ -313,45 +286,40 @@ function getProductWithWagman()
 					axios.get(`${url}/products/${sku}?api-version=2018-10-18${key}`),
 				]);
 		})
-		.then((results) =>
-		{
+		.then((results) => {
 			const product = {
 				sku,
-				pricing: results[ 0 ].data,
-				details: results[ 1 ].data,
+				pricing: results[0].data,
+				details: results[1].data,
 				prices: []
 			};
 
 			// Check to  see if product has an image in wegman API. If not, render a kitty in its place.
-			if (product.details.tradeIdentifiers[ 0 ].images.length === 0)
-			{
+			if (product.details.tradeIdentifiers[0].images.length === 0) {
 				// Change the array to this placeholder image if blank
-				product.details.tradeIdentifiers[ 0 ].images[ 0 ] = 'https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF-650-80.jpg'
+				product.details.tradeIdentifiers[0].images[0] = 'https://cdn.mos.cms.futurecdn.net/VSy6kJDNq2pSXsCzb6cvYF-650-80.jpg'
 			}
 			return product;
 		})
-		.then((product) =>
-		{
-			product[ 'prices' ].push(createRandomPrices(product));
+		.then((product) => {
+			product['prices'].push(createRandomPrices(product));
 			return product;
 		})
 		.catch((e) => console.error(e));
 };
 
 // Randomize prices retrieved in above function; create shuffled array to send to game.ejs
-function createRandomPrices(product)
-{
+function createRandomPrices(product) {
 	const price1 = product.pricing.price;
-	const price2 = _.round(price1 - .2, [ precision = 2 ]);
-	const price3 = _.round(price1 + 1, [ precision = 2 ]);
-	const price4 = _.round(price1 + 2, [ precision = 2 ]);
-	const pricesSet = [ price1, price2, price3, price4 ];
+	const price2 = _.round(price1 - .2, [precision = 2]);
+	const price3 = _.round(price1 + 1, [precision = 2]);
+	const price4 = _.round(price1 + 2, [precision = 2]);
+	const pricesSet = [price1, price2, price3, price4];
 	// Shuffle the array
 	var shuffledPrices = _.shuffle(pricesSet);
 	return shuffledPrices;
 };
 // Hosting on port 5000
-app.listen('5000', function ()
-{
+app.listen('5000', function () {
 	console.log('Listening on port 5000')
 });
